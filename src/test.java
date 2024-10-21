a328943ff97a901204127735463d2cc2657b1cc2
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-10-22 01:44:34 +0200

    Create test.java

---
+++ src/test.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("It's works!");
+
+    }
+}
